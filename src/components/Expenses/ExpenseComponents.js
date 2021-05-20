b1b7f885911b9eafd95d9d311b8e302716cf736b
VictorLira <victorliradev@gmail.com>
2021-05-20 15:39:07 -0300

    adding list of data

--- src/components/Expenses/ExpenseComponents.js
+++ src/components/Expenses/ExpenseComponents.js
@@ -16,27 +16,12 @@ function ExpenseComponents(props) {
     return (
         <div>
             <Card className={styles.expenses}>
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                <ExpenseItem
-                    title={props.item[0].title}
-                    amount={props.item[0].amount}
-                    date={props.item[0].date}
-                ></ExpenseItem>
-                <ExpenseItem
-                    title={props.item[1].title}
-                    amount={props.item[1].amount}
-                    date={props.item[1].date}
-                ></ExpenseItem>
-                <ExpenseItem
-                    title={props.item[2].title}
-                    amount={props.item[2].amount}
-                    date={props.item[2].date}
-                ></ExpenseItem>
-                <ExpenseItem
-                    title={props.item[3].title}
-                    amount={props.item[3].amount}
-                    date={props.item[3].date}
-                ></ExpenseItem>
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {props.item.map(expense => < ExpenseItem
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date} />)
+                }
             </Card>
         </div>
     );
