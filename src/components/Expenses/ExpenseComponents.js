1aa72ffc837794a33409d1907d3a0978220612f2
VictorLira <victorliradev@gmail.com>
2021-05-25 14:20:06 -0300

    adding contitional return statement

--- src/components/Expenses/ExpenseComponents.js
+++ src/components/Expenses/ExpenseComponents.js
@@ -1,33 +1,32 @@
-import ExpenseItem from './ExpenseItem';
-import ExpensesFilter from './ExpensesFilter';
-import styles from './ExpenseComponents.module.css';
-import Card from '../UI/Card';
-import { useState } from 'react';
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import styles from "./ExpenseComponents.module.css";
+import Card from "../UI/Card";
+import { useState } from "react";
+import ExpensesList from "./ExpenseList";
 
 function ExpenseComponents(props) {
-    const [filteredYear, setFilterChangeHandler] = useState('2020');
+  const [filteredYear, setFilterChangeHandler] = useState("2020");
 
-    const filterChangeHandler = function (selectedYear) {
-        setFilterChangeHandler(selectedYear);
-    };
+  const filterChangeHandler = function (selectedYear) {
+    setFilterChangeHandler(selectedYear);
+  };
 
-    const filteredExpenses = props.item.filter(expense => {
-        return expense.date.getFullYear().toString() === filteredYear;
-    })
+  const filteredExpenses = props.item.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
-    return (
-        <div>
-            <Card className={styles.expenses}>
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {filteredExpenses.map(expense => < ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date} />)
-                }   
-            </Card>
-        </div>
-    );
+  return (
+    <div>
+      <Card className={styles.expenses}>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList item={ filteredExpenses }/>
+      </Card>
+    </div>
+  );
 }
 
-export default ExpenseComponents; 
+export default ExpenseComponents;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,19 @@
 import styles from "./ExpenseItem.module.css";
-import React from 'react'
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-
   return (
-    <Card className={styles.expense_item}>
-      <ExpenseDate date={ props.date }/>
-      <div className={styles.expense_item__description}>
-        <h2>{props.title}</h2>
-        <div className={styles.expense_item__price}>${props.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className={styles.expense_item}>
+        <ExpenseDate date={props.date} />
+        <div className={styles.expense_item__description}>
+          <h2>{props.title}</h2>
+          <div className={styles.expense_item__price}>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 }
 
